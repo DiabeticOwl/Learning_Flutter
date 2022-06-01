e79a81bc52a01e0fbba1b72e8737aa6da3eaeaa5
DiabeticOwl <jh.cruz91@gmail.com>
2022-06-01 18:27:25 -0400

    Button Pressed Action implemented.
    The button widget and button pressed action are implemented, allowing the user to introduce some text in the TextWidget and display it by pressing the button.

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -20,20 +20,46 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String text = "";
+
+  // changeText will change the state of the widget, modifying it's text.
+  void changeText(String textForChanging) {
+    setState(() {
+      text = textForChanging;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Hello world! :D")),
-      body: const TextInputWidget(),
+      body: Column(children: <Widget>[
+        TextInputWidget(
+          // changeText is passed as an argument for our TextInputWidget.
+          callback: changeText,
+        ),
+        Text(text)
+      ]),
     );
   }
 }
 
 class TextInputWidget extends StatefulWidget {
-  const TextInputWidget({Key? key}) : super(key: key);
+  // Here TextInputWidget is told that it needs to have this attribute.
+  // Attribute being a function with a String parameter and called "callback".
+  final Function(String) callback;
+
+  // Here TextInputWidget is told that that callback is required at
+  // instantiation.
+  const TextInputWidget({Key? key, required this.callback}) : super(key: key);
 
   @override
   State<TextInputWidget> createState() => _TextInputWidgetState();
@@ -41,7 +67,6 @@ class TextInputWidget extends StatefulWidget {
 
 class _TextInputWidgetState extends State<TextInputWidget> {
   final controller = TextEditingController();
-  String textToWrite = "";
 
   @override
   void dispose() {
@@ -50,30 +75,31 @@ class _TextInputWidgetState extends State<TextInputWidget> {
     controller.dispose();
   }
 
-  void changeText(text) {
-    if (text == "Clear this, please") {
-      controller.clear();
-      text = "";
-    }
-
-    // This will force the class to refresh, allowing the text widget to be
-    // shown.
-    setState(() {
-      textToWrite = text;
-    });
+  // buttonPressed will access the attribute of the TextInputWidget called
+  // "callback" and passes the text of "controller" to it. Then clears the
+  // controller.
+  void buttonPressed() {
+    widget.callback(controller.text);
+    controller.clear();
   }
 
   @override
   Widget build(BuildContext context) {
     return Column(children: <Widget>[
       TextField(
-          controller: controller,
-          decoration: const InputDecoration(
-              prefixIcon: Icon(Icons.message_rounded),
-              labelText: "Write a message here please:"),
-          onChanged: (text) => changeText(text)),
+        controller: controller,
+        decoration: InputDecoration(
+            prefixIcon: const Icon(Icons.message_rounded),
+            suffixIcon: IconButton(
+              // Function "buttonPressed" is passed as the "onPress" action.
+              onPressed: buttonPressed,
+              icon: const Icon(Icons.send),
+              tooltip: "Show Message.",
+              splashColor: Colors.red,
+            ),
+            labelText: "Write a message here please:"),
+      ),
       // Text widget that will show the text.
-      Text(textToWrite)
     ]);
   }
 }
